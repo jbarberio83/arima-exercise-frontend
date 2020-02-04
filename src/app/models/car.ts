749e17aacdfbae973806da880e526bf133f2aa81
Juan Barberio <jbarberio83@gmail.com>
2020-02-04 23:38:34 +0100

    Update car.ts

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,10 +1,10 @@
 export class Car {
-    _id: String;
-    make: String;
-    model: String;
-    price: Number;
-    date: Date;
-    color: String;
-    description: String;
-    vin: String;
+    _id: string;
+    make: string;
+    model: string;
+    price: number;
+    date: date;
+    color: string;
+    description: string;
+    vin: string;
 }
